e675a23dadbae972f2b79c3dc4184e1d022df7ab
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-09-03 13:35:10 -0400

    Add files via upload

---
+++ server/BankingManagementSystem.Core/Models/Account/AccountAllViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingManagementSystem.Core.Models.Account
+{
+    internal class AccountAllViewModel
+    {
+    }
+}
