103017230c8878a34c58103b30515e72f90bd972
Delcho Dichev <del_dichev@abv.bg>
2024-09-05 18:36:56 +0300

    Add Sqlite dependency, configure ApplicationDBContext (#3)
    Co-authored-by: Dichev, Delcho <Delcho.Dichev@experian.com>

--- BankingManagementSystem/Program.cs
+++ BankingManagementSystem/Program.cs
@@ -1,4 +1,7 @@
 
+using BankingManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddApplicationServices();
 builder.Services.AddApplicationDbContext(builder.Configuration);
