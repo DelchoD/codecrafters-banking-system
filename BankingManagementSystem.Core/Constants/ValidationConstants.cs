b4bec3353ba256c2e26277d90e07e16a8f7f532e
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-09-06 13:42:07 -0400

    Add validation constants

---
+++ BankingManagementSystem.Core/Constants/ValidationConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace BankingManagementSystem.Core.Constants
+{
+    public static class ValidationConstants
+    {
+        //Customer
+        public const int CustomerFirstNameMinLength = 2;
+        public const int CustomerFirstNameMaxLength = 50;
+        public const int CustomerMiddleNameMinLength = 2;
+        public const int CustomerMiddleNameMaxLength = 50;
+        public const int CustomerLastNameMinLength = 2;
+        public const int CustomerLastNameMaxLength = 50;
+        public const int CustomerIdNumberMinLength = 10;
+        public const int CustomerIdNumberMaxLength = 10;
+        public const int CustomerAddressMinLength = 10;
+        public const int CustomerAddressMaxLength = 300;
+        public const int CustomerPhoneMaxLength = 10;
+        public const int CustomerPhoneMinLength = 3;
+        public const int CustomerPasswordMinLength = 6;
+
+
+
+
+
+
+        //Account
+        public const int AccountIBANMinLength = 5;
+        public const int AccountIBANMaxLength = 34;
+        public const int AccoutNameMinLength = 4;
+        public const int AccountNameMaxLength = 120;
+
+
+        //Transaction
+        public const int TransactionReasonMinLength = 5;
+        public const int TransactionReasonMaxLength = 500;
+    }
+}
