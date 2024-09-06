a6a32e7add9fbae8bbf8221f284ba91474cfe7b4
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-09-06 13:42:16 -0400

    Add error messages

---
+++ BankingManagementSystem.Core/Constants/ErrorMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace BankingManagementSystem.Core.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string RequireErrorMessage =
+            "Field {0} is required!";
+
+        public const string InvalidFormatErrorMessage =
+            "Field {0} is not in a valid format!";
+
+        public const string StringLengthErrorMessage =
+            "Field {0} must be between {2} and {1} characters long!";
+
+        public const string StringCountCharactersErrorMessage =
+            "Field {0} must be at least {2} characters long!";
+    }
+}
