f1e8a0ad53ae148c6b6dc41c597074df8ff63c6d
OlyaAtanasova <olya.atanasova02@gmail.com>
2024-09-07 17:17:49 +0300

    Implemented methods.

--- BankingManagementSystem.Core/Services/Contracts/ITransactionService.cs
+++ BankingManagementSystem.Core/Services/Contracts/ITransactionService.cs
@@ -1,10 +1,13 @@
-﻿namespace BankingManagementSystem.Core.Services.Contracts
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BankingManagementSystem.Infrastructure.Data.Models;
+namespace BankingManagementSystem.Core.Services.Contracts
 {
     public interface ITransactionService
     {
-        Task GetAllTransactionsAsync();
-        
-
-        //Add more methods
+        Task<List<Transaction>> GetAllTransactionsAsync();
+        Task<Transaction> ProcessTransaction(Transaction transaction);
+        Task<List<Transaction>> GetTransactionsByAccountId(string accountId);
+        Task<Transaction> GetTransactionById(string transactionId);
     }
 }
