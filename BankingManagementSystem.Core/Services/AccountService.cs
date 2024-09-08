215f45467cccbdb0959173570a1c07da31ce32d5
Denis Patronov <piwkillpie@gmail.com>
2024-09-08 16:25:04 +0300

    BS-18: Implemented AccountService.cs (no DTO's yet)
    * BS-18: Added all listed functions. CreateAccount function can be changed to recieve customer id only.
    
    * fixed CreateAccount() function
    
    * BS-18: Made some changes as requested by Angel (except dto's)
    
    * BS-18: Implemented sugested changes
    
    * BS-18: Implemented suggested change
    
    * BS-18: code cleanup

--- BankingManagementSystem.Core/Services/AccountService.cs
+++ BankingManagementSystem.Core/Services/AccountService.cs
@@ -1,21 +1,62 @@
-﻿namespace BankingManagementSystem.Core.Services
+﻿using BankingManagementSystem.Infrastructure.Data.Models;
+
+namespace BankingManagementSystem.Core.Services
 {
     using Contracts;
     using Infrastructure.Data;
 
     public class AccountService : IAccountService
     {
-        private readonly ApplicationDbContext context;
+        private readonly ApplicationDbContext _context;
 
-        public AccountService(ApplicationDbContext _context)
+        public AccountService(ApplicationDbContext context)
         {
-            context = _context;
+            _context = context;
         }
 
-        public async Task GetAllAccountsAsync()
+        public List<Account> GetCustomerAccounts(Customer customer)
         {
-            throw new NotImplementedException();
+            return customer.Accounts.ToList();
         }
 
+        public async Task<Account> CreateAccount(Account account, Customer customer)
+        {
+            if (customer is null)
+                throw new KeyNotFoundException("Customer not found (is null). Cannot create an account");
+
+            account.Customer = customer;
+            await _context.Accounts.AddAsync(account);
+            customer.Accounts.Add(account);
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+            return account;
+        }
+
+        public async Task<Account?> GetAccountById(int accountId)
+        {
+            return await _context.Accounts.FindAsync(accountId);
+        }
+
+        public async Task<Account> UpdateAccountBalance(int accountId, decimal newBalance)
+        {
+            var account = GetAccountById(accountId).Result;
+            if (account is null)
+                throw new KeyNotFoundException($"Account with ID {accountId} not found");
+
+            account.Balance = newBalance;
+            await _context.SaveChangesAsync();
+            return account;
+        }
+
+        public async Task<bool> CloseAccount(int accountId)
+        {
+            var account = GetAccountById(accountId).Result;
+            if (account is null)
+                throw new KeyNotFoundException($"Account with ID {accountId} not found");
+
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
--- BankingManagementSystem.Core/Services/Contracts/IAccountService.cs
+++ BankingManagementSystem.Core/Services/Contracts/IAccountService.cs
@@ -1,10 +1,13 @@
-﻿namespace BankingManagementSystem.Core.Services.Contracts
+﻿using BankingManagementSystem.Infrastructure.Data.Models;
+
+namespace BankingManagementSystem.Core.Services.Contracts
 {
     public interface IAccountService
     {
-        Task GetAllAccountsAsync();
-
-        //Add more methods
-
+        List<Account> GetCustomerAccounts(Customer customer);
+        Task<bool> CloseAccount(int accountId);
+        Task<Account> UpdateAccountBalance(int accountId, decimal newBalance);
+        Task<Account?> GetAccountById(int accountId);
+        Task<Account> CreateAccount(Account account, Customer customer);
     }
 }
