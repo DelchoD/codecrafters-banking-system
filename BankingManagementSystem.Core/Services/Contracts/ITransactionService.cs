4695c7838174cd665a1931d826cacd41c84589be
OlyaAtanasova <olya.atanasova02@gmail.com>
2024-09-10 18:28:57 +0300

    Implemented CRUD operations.

--- BankingManagementSystem.Core/Services/Contracts/ITransactionService.cs
+++ BankingManagementSystem.Core/Services/Contracts/ITransactionService.cs
@@ -7,6 +7,14 @@ namespace BankingManagementSystem.Core.Services.Contracts
         Task<Transaction> ProcessTransaction(Transaction transaction);
         Task<List<Transaction>> GetTransactionsByAccountId(int accountId);
         Task<Transaction> GetTransactionById(int transactionId);
-       
+
+        Task<List<Transaction>> GetTransactionsByIBAN(string iban);
+        Task<List<Transaction>> GetTransactionsByDate(string iban, DateTime startDate, DateTime endDate);
+        Task<List<Transaction>> GetTransactionsByAmount(string iban, decimal minAmount, decimal maxAmount);
+        Task<List<Transaction>> GetOutgoingTransactions(string iban);
+        Task<List<Transaction>> GetIncomingTransactions(string iban);
+        Task<List<Transaction>> GetTransactionsByStatus(string iban, string status);
+        Task<Transaction> UpdateTransactionStatus(int transactionId, string newStatus);
+        Task CancelTransaction(int transactionId);
     }
 }
