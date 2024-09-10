c14bf6fed64e542bc42b55ade7d4e7849c5842e4
LachezarKunov <lachezarkunovbg@gmail.com>
2024-09-10 19:13:44 +0300

    Create a CustomerUpdateDTO which only has attributes that a customer can change

---
+++ BankingManagementSystem.Core/Models/Customer/CustomerUpdateDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingManagementSystem.Core.Models.User
+{
+    using static BankingManagementSystem.Core.Constants.ErrorMessages;
+    using static BankingManagementSystem.Core.Constants.ValidationConstants;
+
+    public class CustomerUpdateDTO
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [EmailAddress(ErrorMessage = InvalidFormatErrorMessage)]
+        public string Email { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(
+            CustomerPhoneMaxLength,
+            MinimumLength = CustomerPhoneMinLength,
+            ErrorMessage = StringLengthErrorMessage
+         )]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [MinLength(CustomerPasswordMinLength, ErrorMessage = StringCountCharactersErrorMessage)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
